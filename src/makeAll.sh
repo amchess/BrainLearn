fcf7041ba0c52646b8c6f21de0be6eeecff741a8
amchess <amchess@libero.it>
2021-11-10 21:49:15 +0100

    BrainLearn 14.2 Tuned eval Author: bmc4 Date: Mon Nov 8 13:14:18 2021 +0100 Timestamp: 1636373658
    Simplify away the Reverse Move penalty
    
    This simplifies the penalty for reverse move introduced in
    https://github.com/official-stockfish/Stockfish/pull/2294 .

--- src/makeAll.sh
+++ src/makeAll.sh
@@ -1,65 +1,115 @@
 make profile-build ARCH=x86-64-vnni COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-vnni'
+mv 'stockfish' 'BrainLearn14-x86-64-vnni'
 make clean
 
 make profile-build ARCH=x86-64-avx512 COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-avx512'
+mv 'stockfish' 'BrainLearn14-x86-64-avx512'
 make clean
 
 make profile-build ARCH=x86-64-bmi2 COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-bmi2'
+mv 'stockfish' 'BrainLearn14-x86-64-bmi2'
 make clean
 
 make profile-build ARCH=x86-64-avx2 COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-avx2'
+mv 'stockfish' 'BrainLearn14-x86-64-avx2'
 make clean
 
 make profile-build ARCH=x86-64-modern COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-modern'
+mv 'stockfish' 'BrainLearn14-x86-64-modern'
 make clean
 
 make profile-build ARCH=x86-64-sse41-popcnt COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-sse41-popcnt'
+mv 'stockfish' 'BrainLearn14-x86-64-sse41-popcnt'
 make clean
 
 make profile-build ARCH=x86-64-ssse3 COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-ssse3'
+mv 'stockfish' 'BrainLearn14-x86-64-ssse3'
 make clean
 
 make profile-build ARCH=x86-64-sse3-popcnt COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64-sse3-popcnt'
+mv 'stockfish' 'BrainLearn14-x86-64-sse3-popcnt'
 make clean
 
 make profile-build ARCH=x86-64 COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-64'
+mv 'stockfish' 'BrainLearn14-x86-64'
 make clean
 
 make profile-build ARCH=general-64 COMP=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-general-64'
+mv 'stockfish' 'BrainLearn14-general-64'
+make clean
+
+make profile-build ARCH=x86-64-vnni COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-vnni'
+make clean
+
+make profile-build ARCH=x86-64-avx512 COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-avx512'
+make clean
+
+make profile-build ARCH=x86-64-bmi2 COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-bmi2'
+make clean
+
+make profile-build ARCH=x86-64-avx2 COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-avx2'
+make clean
+
+make profile-build ARCH=x86-64-modern COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-modern'
+make clean
+
+make profile-build ARCH=x86-64-sse41-popcnt COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-sse41-popcnt'
+make clean
+
+make profile-build ARCH=x86-64-ssse3 COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-ssse3'
+make clean
+
+make profile-build ARCH=x86-64-sse3-popcnt COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64-sse3-popcnt'
+make clean
+
+make profile-build ARCH=x86-64 COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-x86-64'
+make clean
+
+make profile-build ARCH=general-64 COMP=gcc
+strip stockfish
+mv 'stockfish' 'BrainLearn14.2-general-64'
 make clean
 
 make profile-build ARCH=x86-32 COMPCC=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-32'
+mv 'stockfish' 'BrainLearn14.2-x86-32'
 make clean
 
 make profile-build ARCH=x86-32-old COMPCC=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-x86-32-old'
+mv 'stockfish' 'BrainLearn14.2-x86-32-old'
 make clean
 
 make profile-build ARCH=general-32 COMPCC=gcc
 strip stockfish
-mv 'stockfish' 'BrainLearn14.1-general-32'
+mv 'stockfish' 'BrainLearn14.2-general-32'
 make clean
 
