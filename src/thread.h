39c4f04ea15c89a573852d2b6b71c0d4242a8b75
andrea.manzo <andrea.manzo@gitlab.com>
2022-03-05 20:36:32 +0100

    BrainLearn 16 Improved alignment score-gui. Fixed Montecarlo Introduced the option Syzygy50MoveRule, as in Stockfish, useful, effectively, for example, in correspondence chess. Stockfish patch Generalize the feature transform to use vec_t macros This commit generalizes the feature transform to use vec_t macros that are architecture defined instead of using a seperate code path for each one.
    It should make some old architectures (MMX, including improvements by Fanael) faster
    and make further such improvements easier in the future.

--- src/thread.h
+++ src/thread.h
@@ -60,14 +60,11 @@ public:
   Pawns::Table pawnsTable;
   Material::Table materialTable;
   size_t pvIdx, pvLast;
-  RunningAverage doubleExtensionAverage[COLOR_NB];
-  uint64_t nodesLastExplosive;
-  uint64_t nodesLastNormal;
+  RunningAverage complexityAverage;
   std::atomic<uint64_t> nodes, tbHits, bestMoveChanges;
-  Value bestValue;
   int selDepth, nmpMinPly;
   Color nmpColor;
-  ExplosionState state;
+  Value bestValue;//, optimism[COLOR_NB];
 
   Position rootPos;
   StateInfo rootState;
@@ -78,7 +75,7 @@ public:
   ButterflyHistory mainHistory;
   CapturePieceToHistory captureHistory;
   ContinuationHistory continuationHistory[2][2];
-  Score trend;
+  //Score trend;
 };
 
 
