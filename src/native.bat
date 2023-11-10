befd17de2d78dc0bcfc54a1600dfa74d91777916
amchess <amchess@libero.it>
2023-11-10 16:22:23 +0100

    fix native

--- src/native.bat
+++ src/native.bat
@@ -3,6 +3,7 @@ REM x64 builds begin
 SET PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 REM make -j profile-build
 mingw32-make profile-build ARCH=native COMP=mingw -j %Number_Of_Processors%
+strip brainlearn
 ren brainlearn.exe Brainlearn26.2-native.exe
 make clean
 pause
