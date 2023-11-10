94c92e6f19b8c202fb17cfc6af4a9a2c77bd5031
amchess <amchess@libero.it>
2023-11-10 16:22:23 +0100

    fix native

--- src/native.sh
+++ src/native.sh
@@ -1,6 +1,7 @@
 @echo off
 REM make -j profile-build
 make profile-build ARCH=native COMP=gcc
-ren brainlearn.exe Brainlearn26.2-native.exe
+strip brailearn
+ren brainlearn Brainlearn26.2-native.exe
 make clean
 pause
