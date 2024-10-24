075d10f9919097ad8b38070482237eda4b4610d4
padremaurizio4 <padremaurizio4@gmail.com>
2024-10-24 20:37:09 +0200

    Fix versions

--- src/makePopCnt.bat
+++ src/makePopCnt.bat
@@ -4,5 +4,5 @@ SET PATH=C:\tools\msys64\mingw64\bin;C:\tools\msys64\usr\bin;%PATH%
 Title "x86-64-sse41-popcnt"
 mingw32-make profile-build ARCH=x86-64-sse41-popcnt COMP=mingw CXX=x86_64-w64-mingw32-g++ -j %Number_Of_Processors%
 strip brainlearn.exe
-ren brainlearn.exe "BrainlearnSantiago-x86-64-sse41-popcnt.exe"
+ren brainlearn.exe "Brainlearn30-x86-64-sse41-popcnt.exe"
 make clean
